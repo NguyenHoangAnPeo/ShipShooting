cbbc207edbfd3af1499b7a804bceaac7509985d4
HoangAn <hoangan23082004@gmail.com>
2025-09-26 08:55:17 +0700

    GetKeyDown ASDW

--- Assets/Data/InputManager.cs
+++ Assets/Data/InputManager.cs
@@ -4,32 +4,50 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-  [SerializeField] protected float onFire;
-  public float OnFire{get => onFire;}
-     [SerializeField] protected Vector3 worldMousePos;
-     public Vector3 WorldMousePos {get => worldMousePos;}
-  protected static InputManager instance;
-  public static InputManager Instance {get => instance;}
-  private void Awake(){
-    if(InputManager.instance != null){ 
-      Debug.Log("Only 1 InputManager is exits");
+    [SerializeField] protected float onFire;
+    public float OnFire{get => onFire;}
+    [SerializeField] protected Vector3 worldMousePos;
+    public Vector3 WorldMousePos {get => worldMousePos;}
+    protected static InputManager instance;
+    public static InputManager Instance {get => instance;}
+    [SerializeField] protected Vector4 direction;
+    public Vector4 Direction { get => direction; }
+    private void Awake(){
+        if(InputManager.instance != null){ 
+              Debug.Log("Only 1 InputManager is exits");
+            }
+            InputManager.instance = this;
     }
-    InputManager.instance = this;
-  }
-  void Update()
-  {
-    this.GetMouseDown();
-  }
-  void FixedUpdate()
-     {
+    void Update()
+    {
+        this.GetMouseDown();
+        this.GetDirectionByKeyDown();
+    }
+    void FixedUpdate()
+    {
       this.GetMousePos();
-     }
- protected virtual void GetMousePos(){
-     this.worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
- }
- protected virtual void GetMouseDown()
- {
-  this.onFire = Input.GetAxis("Fire1");
- }
+    }
+    protected virtual void GetMousePos(){
+         this.worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    protected virtual void GetMouseDown()
+    {
+        this.onFire = Input.GetAxis("Fire1");
+    }
+    protected virtual void GetDirectionByKeyDown()
+    {
+        this.direction.x = Input.GetKeyDown(KeyCode.A) ? 1 : 0;
+        if(this.direction.x == 0) this.direction.x = Input.GetKeyDown(KeyCode.LeftArrow) ? 1 : 0;
+        this.direction.y = Input.GetKeyDown(KeyCode.D) ? 1 : 0;
+        if(this.direction.y == 0) this.direction.y = Input.GetKeyDown(KeyCode.RightArrow) ? 1 : 0;
+        this.direction.z = Input.GetKeyDown(KeyCode.W) ? 1 : 0;
+        if(this.direction.z == 0) this.direction.z = Input.GetKeyDown(KeyCode.UpArrow) ? 1 : 0;
+        this.direction.w = Input.GetKeyDown(KeyCode.S) ? 1 : 0;
+        if(this.direction.w == 0) this.direction.w = Input.GetKeyDown(KeyCode.DownArrow) ? 1 : 0;
 
+        //if (this.direction.x == 1) Debug.Log("Left");
+        //if(this.direction.y == 1) Debug.Log("Right");
+        //if(this.direction.z == 1) Debug.Log("Up");
+        //if(this.direction.w == 1) Debug.Log("Down");
+    }
 }
